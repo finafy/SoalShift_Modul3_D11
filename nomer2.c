5f1872f0cc36a6cd94ed3cbbe25d3692fbe0653f
EDewangga <elvegadewangga1@gmail.com>
2017-10-25 16:31:13 +0700

    nomer2

--- nomer2.c
+++ nomer2.c
@@ -11,17 +11,17 @@ int lubang1[16]={0},lubang2[16]={0};
 void* player1(void *arg)
 {
 	unsigned long i = 0;
-	int Tebak;
-		tebakan=(int)no;
-		if(lubang1[tebakan-1]==1) {skor1++; printf("pemain 1 menambah nilai\n");}
+	int tebak;
+		tebak=(int)no;
+		if(lubang1[tebak-1]==1) {skor1++; printf("pemain 1 menambah nilai\n");}
 		else {skor2++; printf("pemain 2 menambah nilai\n");}
 }
 void* player2(void *arg);
 {
 	
-	int Tebak;
-	tebakan=(int)no;
-		if(lubang2[tebakan-1]==1) {skor2++; printf("pemain 2 menambah nilai\n");}
+	int tebak;
+	tebak=(int)no;
+		if(lubang2[tebak-1]==1) {skor2++; printf("pemain 2 menambah nilai\n");}
 		else {skor1++; printf("pemain 1 menambah nilai\n");}
 }
 int main()
